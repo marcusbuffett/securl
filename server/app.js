a3384fdfd1f2508351cdd2cc9efdb9997f1418ef
Marcus Buffett <marcusbuffett@me.com>
2015-06-15 13:04:13 -0400

    Log file

--- server/app.js
+++ server/app.js
@@ -2,14 +2,17 @@ var express = require('express');
 var app = express();
 var config = require('./config.js');
 var path = require('path');
-var serveIndex = require('serve-index');
+// var serveIndex = require('serve-index');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var fs = require('fs');
+
+var logStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
 
 mongoose.connect('mongodb://localhost/securl');
 
-app.use(morgan('dev'));
+app.use(morgan('short', {stream: logStream})); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: 'true'
@@ -18,9 +21,6 @@ app.set('view engine', 'jade');
 app.set('views', path.join(config.appDir, 'views'));
 app.use('/public', express.static(path.join(config.appDir, 'public')));
 app.use('/bower_components', express.static(path.join(config.appDir, 'bower_components')));
-// app.use(serveIndex(config.appDir)); 
-
-
 
 var router = require('./api/router.js');
 app.use('/', router); 
